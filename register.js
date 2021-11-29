2e32d22d9c61bfc007487922d9e0764d05a8e5bb
ZsirosLevente <91666510+ZsirosLevente@users.noreply.github.com>
2021-11-29 08:04:28 +0100

    Add files via upload

---
+++ register.js
@@ -0,0 +1,19 @@
+"use strict";
+function registratiton() {
+    let email = document.getElementById("email").value;
+    let telefonszam = document.getElementById("telefonszam").value;
+    let password = document.getElementById("password").value;
+    const validateemail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (email === "" || telefonszam === "" || password === "") {
+        alert("Írj be dolgokat");
+        console.log("Írj be dolgokat  bajnok");
+    }
+    else if (!validateemail.test(email)) {
+        alert("Írj be egy jó email címet bajnok");
+        console.log("Írj be email címet bajnok");
+    }
+    else {
+        alert("A regisztráció sikeres");
+        console.log("Sikeres");
+    }
+}
